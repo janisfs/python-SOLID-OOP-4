491127647618eefb1d126c772c6f103744c079f7
janisfs <janisfs@mail.ru>
2024-07-09 23:41:05 +0300

    add new code

---
+++ Solid-ISP-4.4.py
@@ -0,0 +1,23 @@
+# class SmartHouse():
+#    def turn_on_light(self):
+#        pass
+#
+#    def heat_food(self):
+#        pass
+#
+#    def turn_on_music(self):
+#        pass
+
+class Light():
+    def turn_on_light(self):
+        print("Light is on")
+
+
+class Food():
+    def heat_food(self):
+        print("Food is heating")
+
+
+class Music():
+    def turn_on_music(self):
+        print("Music is on")
